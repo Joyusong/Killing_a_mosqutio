3ed5fc14d87316ea7fe6543e507c0e86b1b6460d
Joyusong <108807708+Joyusong@users.noreply.github.com>
2023-12-13 16:28:53 +0900

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+<<<<<<< HEAD
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+	plugins: [],
+	server: { host: '0.0.0.0', port: 8000 },
+	clearScreen: false,
+})
+=======
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+	plugins: [],
+	server: { host: '0.0.0.0', port: 8000 },
+	clearScreen: false,
+})
+>>>>>>> origin/main
